5bb8682f3a21bb6e4609de982cea11c1d94effbd
Hady Mohamed <hadymohamed@pursuit.org>
2021-05-09 14:57:01 -0400

    added api and calling weather

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    country: "",
+    countryCode: "",
+    temp: "",
+    humidity: "",
+    weather: "",
+    error: "",
+  };
+
+  getWeather = async (event) => {
+    event.preventDefault();
+    const country = event.target.elements.countrySearch.value;
+    const api = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${country}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`,
+    );
+    const data = await api.json();
+
+    if (country !== "") {
+      if (typeof data.name !== "undefined") {
+        this.setState({
+          country: data.name,
+          countryCode: data.sys.country,
+          temp: data.main.temp,
+          humidity: data.main.humidity,
+          weather: data.weather[0].main,
+          error: "",
+        });
+      } else {
+        this.setState({
+          country: "",
+          countryCode: "",
+          temp: "",
+          humidity: "",
+          weather: "",
+          error: "Invalid Search",
+        });
+      }
+    } else {
+      this.setState({
+        country: "",
+        countryCode: "",
+        temp: "",
+        humidity: "",
+        weather: "",
+        error: "Enter Country Or City Name To Search",
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.getWeather}>
+          <input type="text" name="countrySearch" />
+          <button type="submit">Search</button>
+        </form>
+        <div className="Container">
+          <div className="Temp">
+            {this.state.temp && <span>{Math.round(this.state.temp)}°F,</span>}
+            {this.state.weather && <span>{this.state.weather}</span>}
+            {this.state.humidity && <span>{this.state.humidity}%</span>}
+          </div>
+          <div className="Country-Info">
+            {this.state.country && <span>{this.state.country},</span>}
+            {this.state.countryCode && <span>{this.state.countryCode}</span>}
+          </div>
+          <div className="Error">
+            {this.state.error && <span>{this.state.error}</span>}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
